9311381eb7db475cc77ef1f33bb34c4209505fdf
Alan <alan.y.han@gmail.com>
2020-08-31 23:46:12 +0100

    Updates

---
+++ precache-manifest.28bc4545644512b49583346a5287c3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d3dba37b6fa6736cb3bedb9fec67b4",
+    "url": "/waifu2gif/index.html"
+  },
+  {
+    "revision": "94c6fa87ef8ced1e0d05",
+    "url": "/waifu2gif/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2eb6139335962ab0f9c1",
+    "url": "/waifu2gif/static/js/2.2e5aa6d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/waifu2gif/static/js/2.2e5aa6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c6fa87ef8ced1e0d05",
+    "url": "/waifu2gif/static/js/main.50ed3177.chunk.js"
+  },
+  {
+    "revision": "bd631038afab765879da",
+    "url": "/waifu2gif/static/js/runtime-main.218dadb7.js"
+  }
+]);
